9939ec477f1a6e21aef2faa9a98e13bb9f929000
MARIA EDUARDA PEDROSO <mpedroso@alunos.utfpr.edu.br>
2025-12-10 04:28:18 +0000

    porque tem esse rolagem interna na pagina???

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -15,7 +15,7 @@ export default function DashboardLayout({
         <AppSidebar />
         <div className="flex-1 flex flex-col min-w-0">
           <AppHeader />
-          <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background overflow-y-auto">
+          <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
             <div className="mx-auto w-full max-w-7xl">
               {children}
             </div>
